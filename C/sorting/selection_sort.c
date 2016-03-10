a0139fcd6efe1631fba711361b31d4458ad9dca7
Roberto Esposito <roberto.esposito@unito.it>
2016-03-10 16:24:20 +0100

    Improved comments
    Commented helper functions in selection_sort.c

--- C/sorting/selection_sort.c
+++ C/sorting/selection_sort.c
@@ -1,5 +1,13 @@
 #include "selection_sort.h"
 
+
+/*
+ * Restituisce l'indice del minimo elemento in array[pos..(size-1)].
+ * array: l'array in cui cercare
+ * size: dimensione dell'array
+ * pos: posizione da cui far iniziare la ricerca
+ * compare: funzione di confronto tra due elementi dell'array
+ */
 int select(void** array, int size, int pos, CompFunction compare) {
   int min_index = pos;
 
@@ -12,6 +20,11 @@ int select(void** array, int size, int pos, CompFunction compare) {
   return min_index;
 }
 
+/*
+ * Dati due puntatori, ne inverte il contenuto
+ * e1: puntatore al primo valore
+ * e2: puntatore al secondo valore
+ */
 void swap(void** e1, void** e2) {
   void* tmp = *e1;
   *e1 = *e2;
