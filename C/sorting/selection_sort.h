fdac2ad7a14eae2c44079e3236f12de53fc3e70c
Roberto Esposito <roberto.esposito@unito.it>
2016-03-14 16:14:28 +0100

    Updated selection sort example.
    Created a very minimal test library and used in selection_sort_test.c.
    Updated documentation selection_sort.h to emphasize the intended usage of
    the selection_sort function.
    Updated selection_sort_test with a new test over arrays of pointers and
    commented about using long int as bad practice.

--- C/sorting/selection_sort.h
+++ C/sorting/selection_sort.h
@@ -7,6 +7,10 @@ typedef int (*CompFunction)(void*, void*);
  * Implementa il selection selection_sort sul posto
  * array: l'array da ordinare
  * compare: funzione per valutare l'ordine di due elementi dell'array
+ *
+ * Nota: la funzione va usata per confrontare elementi che possono essere
+ *   dereferenziati. L'utilizzo di essa per ordinare array di tipi "nativi"
+ *   non è supportato e andrebbe evitato.
  */
 void selection_sort(void** array, int size, CompFunction compare);
 
