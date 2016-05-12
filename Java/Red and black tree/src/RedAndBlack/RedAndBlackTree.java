e8d7d794e89462c51c1fe9d891b4021799f62eda
Lorenzo Gandino <lorenzoga94@gmail.com>
2016-05-12 16:29:48 +0200

    Modifiche alla classe per estendere la classe Dictionary

--- Java/Red and black tree/src/RedAndBlack/RedAndBlackTree.java
+++ Java/Red and black tree/src/RedAndBlack/RedAndBlackTree.java
@@ -1,101 +1,118 @@
 package RedAndBlack;
 
 import java.util.Comparator;
-/**
- *	
- * @param <T>
- */
-public class RedAndBlackTree<T> {
-	/**
-	 *
-	 */
-	private class Node {
-		private T value;
-		/*
-		 * color black: true;
-		 * color red: false;
-		 */
-		private boolean color = true;
-		private Node father = null;
-		private Node leftChild = null;
-		private Node rightChild = null;
-		
-		public Node(T value, boolean color, Node father, Node left, Node right){
-			setValue(value);
-			setColor(color);
-			setFather(father);
-			setLeft(left);
-			setRight(right);
-		}
-		
-		public Node(T value, boolean color){
-			this(value, color,null, null, null);
-		}
-		
-		public T getValue(){
-			return this.value;
-		}
-		public boolean getColor(){
-			return this.color;
-		}
-		public Node getFather(){
-			return father;
-		}
-		public Node getLeft(){
-			return leftChild;
-		}
-		public Node getRight(){
-			return rightChild;
-		}
-		public void setValue(T value){
-			this.value = value;
-		}
-		public void setColor(boolean c){
-			this.color = c;
-		}
-		public void setFather(Node father){
-			this.father = father;
-		}
-		public void setLeft(Node left){
-			this.leftChild = left;
-		}
-		public void setRight(Node right){
-			this.rightChild = right;
-		}
+import java.util.Dictionary;
+import java.util.Enumeration;
+
+public class RedAndBlackTree<K, V> extends Dictionary<K, V> {
+    private enum Color {
+	RED, BLACK;
+    }
+
+    private class Node {
+	public V value;
+	public K key;
+	public Color color;
+	public Node father = null;
+	public Node leftChild = null;
+	public Node rightChild = null;
+
+	public Node(V value, Color color, Node father, Node left, Node right) {
+	    this.value = value;
+	    this.color = color;
+	    this.father = father;
+	    this.leftChild = left;
+	    this.rightChild = right;
 	}
 
-	/*
-	 * Attribute:
-	 */
-	
-	private T root = null;
-	private Comparator<T> comparator;
-	private int size = 0;
-	
-	public RedAndBlackTree(Comparator<T> comp){
-		setComparator(comp);
+	@SuppressWarnings("unused")
+	public Node(V value, Color color) {
+	    this(value, color, null, null, null);
 	}
-	
-	public void setComparator(Comparator<T> c){
-		this.comparator = c;
-	}
-	
-	public void add(T value){
-		//Code to do
-		this.size++;
-	}
-	public boolean remove(T value){
-		// Code to do
-		this.size--;
-		return true;
-	}
-	
-	private void leftRotate(Node nodo){
-		
-	}
-	
-	private void rightRotate(Node nodo){
-		
-	}
-	
+    }
+
+    private Node root;
+    private Comparator<K> comparator;
+    private int size = 0;
+
+    public RedAndBlackTree(Comparator<K> comp) {
+	setComparator(comp);
+	root = null;
+    }
+
+    public void setComparator(Comparator<K> c) {
+	this.comparator = c;
+    }
+
+    public void add(V value) {
+	// Code to do
+	this.size++;
+    }
+
+    private void leftRotate(Node nodo) {
+	// Sinistra: true
+	// Destra: false
+	boolean verso;
+	Node padre = nodo.father;
+
+	verso = padre.leftChild == nodo;
+	if (padre != null)
+	    if (verso)
+		padre.leftChild = nodo.rightChild;
+	    else
+		padre.rightChild = nodo.rightChild;
+	else
+	    this.root = nodo.rightChild;
+	Node p = nodo.rightChild.leftChild;
+	nodo.rightChild.leftChild = nodo;
+	nodo.rightChild = p;
+
+    }
+
+    private void rightRotate(Node nodo) {
+
+    }
+
+    @Override
+    public Enumeration<V> elements() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+    @Override
+    public Enumeration<K> keys() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public V get(Object key) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public int size() {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
 }
