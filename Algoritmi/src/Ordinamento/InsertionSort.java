52020119179738dc0ee617eb61b68ee1560857cd
Lorenzo Gandino <lorenzoga94@gmail.com>
2016-05-21 15:41:20 +0200

    Modifica sort(ArrayList<T>)
    Task-Url:

--- Algoritmi/src/Ordinamento/InsertionSort.java
+++ Algoritmi/src/Ordinamento/InsertionSort.java
@@ -14,12 +14,10 @@ class InsertionSort<T> {
     public void sort(ArrayList<T> arr) {
 	for (int i = 1; i < arr.size(); i++) {
 	    T p = arr.get(i);
-	    {
-		int j;
-		for (j = i - 1; j > -1 && comparator.compare(arr.get(j), p) > 0; j--)
-		    arr.set(j + 1, arr.get(j));
-		arr.set(j + 1, p);
-	    }
+	    int j;
+	    for (j = i - 1; j != 0 && comparator.compare(arr.get(j), p) > 0; j--)
+		arr.set(j + 1, arr.get(j));
+	    arr.set(j + 1, p);
 	}
     }
 
@@ -29,17 +27,9 @@ class InsertionSort<T> {
 
     }
 
-    /*
-     * public boolean isSorted(ArrayList<T> a){ for(int i=1;i<a.size();i++){
-     * if((int)a.get(i) >= (int)a.get(i-1)){ continue; } else return false; }
-     * return true;
-     * 
-     * }
-     */
-
     public static <T extends Comparable<? super T>> boolean isSorted(ArrayList<T> a) {
 	for (int i = 1; i < a.size(); i++) {
-	    if (a.get(i).compareTo(a.get(i - 1)) == 0 || a.get(i).compareTo(a.get(i - 1)) == 1) {
+	    if (a.get(i).compareTo(a.get(i - 1)) >= 0) {
 		continue;
 	    } else
 		return false;
