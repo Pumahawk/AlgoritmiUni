34a0a99fe87ae47ba6b45f3d3a1ec2eae4d1faa7
Lorenzo Gandino <lorenzoga94@gmail.com>
2016-05-22 21:02:40 +0200

    Test CVS e aggiunta al progetto il file csv
    Task-Url:

--- Algoritmi/src/Ordinamento/ReadCVS.java
+++ Algoritmi/src/Ordinamento/ReadCVS.java
@@ -5,32 +5,44 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import Ordinamento.RecordCSV.Type;
+
 // TODO da testare
 public class ReadCVS {
 
-    private class Record {
-	public String s;
-	public Integer i;
-	public Float f;
-
-	public Record(String string, Integer integer, Float fl) {
-	    this.s = string;
-	    this.f = fl;
-	    this.i = integer;
-	}
-    }
-
-    private ArrayList<Record> table;
+    private ArrayList<RecordCSV> table;
 
     public ReadCVS(File fileCVS, int min, int max) throws FileNotFoundException {
-	table = new ArrayList<>(max - min);
+	/*
+	 * if (max - min > 0) table = new ArrayList<>(max - min); else table =
+	 * new ArrayList<>(20000000);
+	 */
+	table = new ArrayList<>();
 	Scanner sc = new Scanner(fileCVS);
-	for (int i = 0; i <= min; i++)
-	    sc.skip("\n");
-	for (int i = min; sc.hasNextLine() && i <= max; i++) {
+	for (int i = 0; i < min && sc.hasNextLine(); i++)
+	    sc.nextLine();
+	for (int i = min; sc.hasNextLine() && i != max + 1; i++) {
 	    String[] line = sc.nextLine().split(",");
-	    table.add(new Record(line[1], Integer.parseInt(line[2]), Float.parseFloat(line[3])));
+	    table.add(new RecordCSV(line[1], Integer.parseInt(line[2]), Float.parseFloat(line[3])));
 	}
 	sc.close();
     }
+
+    public ArrayList<RecordCSV> getTable() {
+	return table;
+    }
+
+    public static void main(String args[]) throws FileNotFoundException {
+	File cvs = new File("Data" + File.separator + "records.csv");
+	ReadCVS table = new ReadCVS(cvs, 0, 1000000 - 1);
+	QuickSort<RecordCSV> ording = new QuickSort<>(RecordCSV.getComparator(Type.STRING));
+
+	System.out.println("Inizio ordinamento");
+
+	ording.quickSort(table.getTable());
+	// for (RecordCSV rec : table.getTable())
+	// System.out.println(rec.s);
+	System.out.println("Fine ordinamento: " + ording.isSorted(table.getTable()));
+    }
+
 }
---
+++ Algoritmi/src/Ordinamento/RecordCSV.java
@@ -0,0 +1,31 @@
+package Ordinamento;
+
+import java.util.Comparator;
+
+public class RecordCSV {
+    public String s;
+    public Integer i;
+    public Float f;
+
+    public enum Type {
+	STRING, INTEGER, FLOAT
+    }
+
+    public RecordCSV(String string, Integer integer, Float fl) {
+	this.s = string;
+	this.f = fl;
+	this.i = integer;
+    }
+
+    public static Comparator<RecordCSV> getComparator(Type type) {
+	switch (type) {
+	case STRING:
+	    return (RecordCSV a, RecordCSV b) -> a.s.compareTo(b.s);
+	case INTEGER:
+	    return (RecordCSV a, RecordCSV b) -> a.i.compareTo(b.i);
+	case FLOAT:
+	    return (RecordCSV a, RecordCSV b) -> a.f.compareTo(b.f);
+	}
+	return null;
+    }
+}
