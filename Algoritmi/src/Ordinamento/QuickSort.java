6df538a5f9fa3826f81b5150638e3876dfd400e5
Lorenzo Gandino <lorenzoga94@gmail.com>
2016-05-22 21:07:11 +0200

    Conversione parametro
    Il parametro da array di t è diventato un ArrayList

--- Algoritmi/src/Ordinamento/QuickSort.java
+++ Algoritmi/src/Ordinamento/QuickSort.java
@@ -1,5 +1,6 @@
 package Ordinamento;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 
 public class QuickSort<T> {
@@ -9,20 +10,20 @@ public class QuickSort<T> {
 	this.comparator = comparator;
     }
 
-    public void quickSort(T[] a) {
-	partition(a, 0, a.length - 1);
+    public void quickSort(ArrayList<T> a) {
+	partition(a, 0, a.size() - 1);
     }
 
-    public void partition(T[] a, int start, int end) {
+    public void partition(ArrayList<T> a, int start, int end) {
 	if (start < end) {
 	    int pivot = (start + end) / 2;
-	    T x = a[pivot];
+	    T x = a.get(pivot);
 	    scambia(a, pivot, start);
 	    int leftPointer = start, rightPointer = end;
 	    while (leftPointer <= rightPointer) {
-		while (comparator.compare(a[leftPointer], x) < 0)
+		while (comparator.compare(a.get(leftPointer), x) < 0)
 		    leftPointer++;
-		while (comparator.compare(a[rightPointer], x) > 0)
+		while (comparator.compare(a.get(rightPointer), x) > 0)
 		    rightPointer--;
 		if (leftPointer <= rightPointer) {
 		    scambia(a, leftPointer, rightPointer);
@@ -36,18 +37,25 @@ public class QuickSort<T> {
 
     }
 
-    public void scambia(T[] a, int i, int j) {
-	T temp = a[i];
-	a[i] = a[j];
-	a[j] = temp;
+    public void scambia(ArrayList<T> a, int i, int j) {
+	T temp = a.get(i);
+	a.set(i, a.get(j));
+	a.set(j, temp);
+    }
+
+    public boolean isSorted(ArrayList<T> a) {
+	for (int i = 1; i < a.size(); i++)
+	    if (comparator.compare(a.get(i - 1), a.get(i)) > 0)
+		return false;
+	return true;
     }
 
     public static void main(String args[]) {
-	Integer[] arr = new Integer[1000000];
-	for (int i = 0; i < arr.length; i++)
-	    arr[i] = (int) (Math.random() * 100000 - 1000);
+	ArrayList<Integer> arr = new ArrayList<Integer>();
+	for (int i = 0; i < arr.size(); i++)
+	    arr.set(i, (int) (Math.random() * 100000 - 1000));
 	QuickSort<Integer> qs = new QuickSort<Integer>((Integer a, Integer b) -> (a < b) ? -1 : (a == b) ? 0 : 1);
-	qs.partition(arr, 0, arr.length - 1);
+	qs.partition(arr, 0, arr.size() - 1);
 	return;
     }
 }
