4799e883434dfb951b08c426b95b6b921340781a
Lorenzo Gandino <lorenzoga94@gmail.com>
2016-05-22 16:56:59 +0200

    Modifica classe ReadCVS
    Ora la classe è piu facile da laggere e modificare e riutilizzare in
    altri classi

--- Algoritmi/src/Ordinamento/ReadCVS.java
+++ Algoritmi/src/Ordinamento/ReadCVS.java
@@ -1,76 +1,36 @@
 package Ordinamento;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Scanner;
 
+// TODO da testare
 public class ReadCVS {
 
-    private String s;
-    private int i;
-    private float f;
+    private class Record {
+	public String s;
+	public Integer i;
+	public Float f;
 
-    private static class IntegerComparator implements Comparator<Integer> {
-	@Override
-	public int compare(Integer i1, Integer i2) {
-	    return i1.compareTo(i2);
+	public Record(String string, Integer integer, Float fl) {
+	    this.s = string;
+	    this.f = fl;
+	    this.i = integer;
 	}
     }
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    private ArrayList<Record> table;
 
-	ReadCVS obj = new ReadCVS();
-	obj.run();
-
-    }
-
-    public void run() throws NumberFormatException, IOException {
-
-	String csvFile = "C://Users/UMIT AHMED/workspace/Algoritmi/src/Ordinamento/records.csv";
-	BufferedReader br = null;
-	String line = "";
-	String cvsSplitBy = ",";
-	int i = 0;
-	ArrayList<String> field1 = new ArrayList<String>();
-	ArrayList<Integer> field2 = new ArrayList<Integer>();
-	ArrayList<Float> field3 = new ArrayList<Float>();
-	ArrayList<ReadCVS> field = new ArrayList<ReadCVS>();
-	try {
-
-	    br = new BufferedReader(new FileReader(csvFile));
-	    while ((line = br.readLine()) != null) {
-
-		// use comma as separator
-		String[] rec = line.split(cvsSplitBy);
-		ReadCVS e = new ReadCVS();
-		e.s = rec[1];
-		e.i = Integer.parseInt(rec[2]);
-		e.f = Float.parseFloat(rec[3]);
-		System.out.println(e.i);
-		field.add(e);
-
-		field1.add(e.s);
-		field2.add(e.i);
-		field3.add(e.f);
-
-		i++;
-
-	    }
-
-	} catch (FileNotFoundException e) {
-	    e.printStackTrace();
+    public ReadCVS(File fileCVS, int min, int max) throws FileNotFoundException {
+	table = new ArrayList<>(max - min);
+	Scanner sc = new Scanner(fileCVS);
+	for (int i = 0; i <= min; i++)
+	    sc.skip("\n");
+	for (int i = min; sc.hasNextLine() && i <= max; i++) {
+	    String[] line = sc.nextLine().split(",");
+	    table.add(new Record(line[1], Integer.parseInt(line[2]), Float.parseFloat(line[3])));
 	}
-
-	System.out.println("Numero degli elementi da ordinare" + i + "\n");
-	QuickSort<Integer> a = new QuickSort<Integer>(new IntegerComparator());
-	a.print2(field2);
-	System.out.println("QuickSort \n");
-	// a.sort(field2);
-	// a.print(field2);
-
+	sc.close();
     }
-
 }
